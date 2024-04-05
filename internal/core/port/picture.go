d3a5addd1701f7d86ac1fb1dc4d3a9de09251aa8
fullgukbap <2hanjunbum6@gmail.com>
2024-04-05 12:26:40 +0900

    faet: picture port 구현 완료

---
+++ internal/core/port/picture.go
@@ -0,0 +1,22 @@
+package port
+
+import (
+	"context"
+	"letsgo-mini-is/internal/core/domain"
+
+	"github.com/google/uuid"
+)
+
+type PictureService interface {
+	Create(context.Context, *domain.Picture) (*domain.Picture, error)
+	FindBy(context.Context, uuid.UUID) (*domain.Picture, error)
+	Update(context.Context, *domain.Picture) (*domain.Picture, error)
+	Delete(context.Context, uuid.UUID) error
+}
+
+type PictureRepository interface {
+	Create(context.Context, *domain.Picture) (*domain.Picture, error)
+	FindBy(context.Context, uuid.UUID) (*domain.Picture, error)
+	Update(context.Context, *domain.Picture) (*domain.Picture, error)
+	Delete(context.Context, uuid.UUID) error
+}
