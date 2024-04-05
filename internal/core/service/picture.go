c152e8011e7e6ae632a1e88bdbf7da6ad54718e9
fullgukbap <2hanjunbum6@gmail.com>
2024-04-05 13:53:04 +0900

    feat: service 로직 수정

---
+++ internal/core/service/picture.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+	"letsgo-mini-is/internal/core/domain"
+	"letsgo-mini-is/internal/core/port"
+
+	"github.com/google/uuid"
+)
+
+type PictureService struct {
+	pictureRepository *port.PictureRepository
+}
+
+func NewPictureService(pictureRepository *port.PictureRepository) *PictureService {
+	return &PictureService{
+		pictureRepository: pictureRepository,
+	}
+}
+
+func (s *PictureService) Create(c context.Context, p *domain.Picture) (*domain.Picture, error) {
+	return s.Create(c, p)
+}
+
+func (s *PictureService) Find(c context.Context, id uuid.UUID) (*domain.Picture, error) {
+	return s.Find(c, id)
+}
+
+func (s *PictureService) Update(c context.Context, p *domain.Picture) (*domain.Picture, error) {
+	return s.Update(c, p)
+}
+
+func (s *PictureService) Delete(c context.Context, id uuid.UUID) error {
+	return s.Delete(c, id)
+}
