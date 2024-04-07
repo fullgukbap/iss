e3cf756ab4ae584b6ad24f618f73865a0f3bc931
fullgukbap <2hanjunbum6@gmail.com>
2024-04-07 14:00:15 +0900

    feat: config 관리자 추가

---
+++ internal/adapter/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Container struct {
+	DB   *DB
+	HTTP *HTTP
+}
+
+type DB struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	Database string
+}
+
+type HTTP struct {
+	Port string
+}
+
+func New() (*Container, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	HTTP := &HTTP{
+		Port: os.Getenv("HTTP_PORT"),
+	}
+
+	DB := &DB{
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Database: os.Getenv("DB_DATABASE"),
+	}
+
+	return &Container{
+		HTTP: HTTP,
+		DB:   DB,
+	}, nil
+}
