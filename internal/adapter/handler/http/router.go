ae11ea082ac465cae0b4cfcb1b4fe1b6132635a0
fullgukbap <2hanjunbum6@gmail.com>
2024-04-07 14:04:29 +0900

    feat: router 개발 완료

--- internal/adapter/handler/http/router.go
+++ internal/adapter/handler/http/router.go
@@ -3,12 +3,13 @@ package http
 import (
 	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/etag"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
@@ -16,17 +17,17 @@ type Router struct {
 	*fiber.App
 }
 
-func NewRouter(pictureHandler PictureHandler) (*Router, error) {
+func NewRouter(pictureHandler *PictureHandler) (*Router, error) {
 	app := fiber.New(fiber.Config{
 		ErrorHandler: customErrorHandler,
 	})
 	app.Use(recover.New())
 	app.Use(helmet.New())
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Content-Length", "Accept-Language", "Accept-Encoding", "Connection", "Access-Control-Allow-Origin"},
-		AllowOrigins:     []string{"*"},
-		AllowCredentials: true,
-		AllowMethods:     []string{"GET", "POST", "HEAD", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		// AllowOrigins:     "*",
+		// AllowCredentials: true,
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 
 	app.Use(limiter.New(limiter.Config{
@@ -40,12 +41,23 @@ func NewRouter(pictureHandler PictureHandler) (*Router, error) {
 	}))
 	app.Use(etag.New())
 
-	app.Post("/api/picutre", pictureHandler.Create)
-	app.Get("/api/picutre", pictureHandler.Find)
-	app.Put("/api/picutre", pictureHandler.Update)
-	app.Delete("/api/picutre", pictureHandler.Delete)
+	// Initialize default config
+	app.Use(logger.New())
 
-	var r *Router
-	r.App = app
-	return r, nil
+	// Or extend your config for customization
+	// Logging remote IP and Port
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+
+	app.Post("/api/picture", pictureHandler.Create)
+	app.Get("/api/picture/:id", pictureHandler.Find)
+	app.Put("/api/picture", pictureHandler.Update)
+	app.Delete("/api/picture", pictureHandler.Delete)
+
+	return &Router{App: app}, nil
+}
+
+func (r *Router) Run(port string) error {
+	return r.Listen(port)
 }
