7ea81664112750d1c49b233ac8cda6e582b52252
fullgukbap <2hanjunbum6@gmail.com>
2024-04-07 13:58:52 +0900

    fix: Picture 도메인 구조 추가

--- internal/core/domain/picture.go
+++ internal/core/domain/picture.go
@@ -3,13 +3,15 @@ package domain
 import "github.com/google/uuid"
 
 type Picture struct {
-	ID      uuid.UUID `json:"id"`
-	Content []byte    `json:"content"`
+	ID        uuid.UUID `json:"id"`
+	Content   []byte    `json:"content"`
+	Extension string    `json:"extension"`
 }
 
-func NewPicture(id uuid.UUID, content []byte) *Picture {
+func NewPicture(id uuid.UUID, content []byte, extension string) *Picture {
 	return &Picture{
-		ID:      id,
-		Content: content,
+		ID:        id,
+		Content:   content,
+		Extension: extension,
 	}
 }
